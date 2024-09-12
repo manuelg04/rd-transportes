db0a2408063b4a7ec1a1153cec1c1b40549440ed
Manuel Gonzalez <manuelg9704@gmail.com>
2024-09-12 09:08:01 -0500

    redeploy page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { ValuesBanner } from "./components/Banner";
 import { TrustedClients } from "./components/TrustedClients";
 import { useInView } from 'react-intersection-observer';
 import { ReactNode } from 'react';
-import ServiceNotAvailable from "./components/ServiceNotAvailable";
 
 interface AnimatedSectionProps {
   children: ReactNode;
@@ -36,8 +35,7 @@ const AnimatedSection = ({ children, className }: AnimatedSectionProps) => {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <ServiceNotAvailable />
-      {/* <Hero />
+      <Hero />
 
       <AnimatedSection className="w-full py-4 bg-gray-50">
         <ValuesBanner />
@@ -49,7 +47,7 @@ export default function Home() {
 
       <AnimatedSection className="w-full py-4 bg-white">
         <EnhancedCarousel />
-      </AnimatedSection> */}
+      </AnimatedSection>
     </main>
   );
 }
